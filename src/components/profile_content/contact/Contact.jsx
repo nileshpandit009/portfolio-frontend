3af8f50e3de499a557062caddb2d97b2ec13d819
Nilesh Pandit <nileshpandit009@gmail.com>
2021-06-05 22:49:49 +0530

    Corrected email form

--- src/components/profile_content/contact/Contact.jsx
+++ src/components/profile_content/contact/Contact.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ContactInfo from "./ContactInfo";
 import Form from "./Form";
 import "./contact.css";
 
 function Contact(props) {
-  const [sent, setSent] = useState(false);
-
   return (
     <div id={props.id} className="content-root">
       <div className="section-container">
         <span className="section-header-text">Contact me</span>
         <div className="section-content-text contact">
-          <Form sent={sent} setSent={setSent} />
+          <Form />
           <ContactInfo />
         </div>
       </div>
